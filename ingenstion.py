c73c0307c3e73e74a54adfa605a9f3a4234afcf5
unknown <yimingsu2007@gmail.com>
2025-10-10 14:32:08 -0700

    Add wiping of old data and more logging to ingenstion.py

--- ingenstion.py
+++ ingenstion.py
@@ -1,6 +1,24 @@
-import requests
 import pandas as pd
+import os
+import numpy as np
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
 
 df = pd.read_csv('https://opendata.vancouver.ca/api/explore/v2.1/catalog/datasets/public-trees/exports/csv', on_bad_lines='skip', sep=';')
-print(df.head())
+df[["latitude", "longitude"]] = df["geo_point_2d"].str.split(",", expand=True).astype(float)
+df = df.replace(r'^\s*$', np.nan, regex=True)
+
+df = df.drop(columns=["geo_point_2d", "geom"])
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+with engine.begin() as conn:
+    print("Wiping old data")
+    conn.execute(text("TRUNCATE TABLE trees;"))
+    
+    
+
+print("Data ingestion complete!")
