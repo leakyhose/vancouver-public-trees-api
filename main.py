e780432ac8c63b8c6f66b6fc92a31bba7607e746
unknown <yimingsu2007@gmail.com>
2025-10-17 09:35:18 -0700

    Add database connection setup

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 
 @app.get("/")
