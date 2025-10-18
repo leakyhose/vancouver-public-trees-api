5dcc5e12fabbd208d1080be442406649ffd2a1ed
unknown <yimingsu2007@gmail.com>
2025-10-18 15:27:56 -0700

    Add health endpoint

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/api/v1/health")
+async def health():
+    return {"status": "ok"}
