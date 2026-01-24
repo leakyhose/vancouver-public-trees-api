fed4fac86189000a3081d86bd2387025f62932d3
unknown <yimingsu2007@gmail.com>
2026-01-24 15:36:59 -0800

    Moved redis cache to module leves

--- api/species.py
+++ api/species.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from sqlalchemy import text
 from .db import engine
+from redis_cache import redis_cache
 
 router = APIRouter(prefix="/api/v1/species", tags=["species"])
 
-
+@redis_cache(expire_seconds=3600)
 @router.get("")
 async def list_species():
     with engine.connect() as conn:
